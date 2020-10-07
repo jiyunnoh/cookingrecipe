e8cefe96ffddf06734bc6a17fdf374cbc18658d0
Jiyun Noh <ianjy1127@gmail.com>
2020-10-07 15:36:11 -0500

    create recipe.html

--- recipeapp/views.py
+++ recipeapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Cuisine, Dietary, Difficulty, DishType, Occassion, Rating, Recipe, Review, Comment, Ingredient, Favorite
 
 # Create your views here.
 def index (request):
     return render(request, 'recipeapp/index.html')
+
+def getrecipe(request):
+    recipe_list=Recipe.objects.all()
+    return render(request, 'recipeapp/recipe.html' ,{'recipe_list' : recipe_list})
     
