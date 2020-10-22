962cb3580ef6642fb5ea1b626bccb5476006cc1e
eunicejlee <43863945+eunicejlee@users.noreply.github.com>
2020-10-22 13:26:38 -0700

    Update urls.py
    Added login and logout.

--- recipeapp/urls.py
+++ recipeapp/urls.py
@@ -10,4 +10,6 @@ urlpatterns=[
     path('dietary/', views.getdietary, name='dietary'),
     path('cuisine/', views.getcuisine, name='cuisine'),
     path('dishtype/', views.getdishtype, name='dishtype'),
+    path('loginmessage/', view.loginmessage, name='loginmessage'),
+    path('logoutmessage/', view.logoutmessage, name='logoutmessage),     
 ]
